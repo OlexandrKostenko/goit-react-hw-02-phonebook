5627aca54bfb8e955cef1e906b654362c90b3952
OlexandrKostenko <106703542+OlexandrKostenko@users.noreply.github.com>
2023-01-14 22:25:37 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,6 @@ import { nanoid } from 'nanoid';
 import { Form } from "./Form/Form";
 import { ContactsList } from "./ContactsList/ContactsList";
 import { Filter } from "./Filter/Filter";
-import { element } from "prop-types";
 import { Wrapper } from "./App.styled";
 
 
